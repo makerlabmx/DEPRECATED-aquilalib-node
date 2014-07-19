b02687dbb48b1f73cbf62e63dce55c039e31bce6
Rodrigo Méndez <rodmg4@gmail.com>
2014-07-19 01:17:39 -0500

    Now Automatically detects the bridge port. Posibility to append events with '.on', etc in Aq()

--- config.js
+++ config.js
@@ -6,7 +6,6 @@ var config = {};
 
 // Bridge connection parameters:
 //config.PORT = "/dev/tty.usbmodem2642";
-config.PORT = "/dev/tty.usbserial-DA00RNZ1";
 config.BAUDRATE = 57600;
 
 module.exports = config;
